9ebce4d69267201f29f0a2945c0d037a90e766b7
Malireddy Naveen Kumar Reddy <naveen.malireddy@artihcus.com>
2024-09-09 13:45:45 +0000

    Naveen

--- webapp/controller/Supervisor.controller.js
+++ webapp/controller/Supervisor.controller.js
@@ -1484,6 +1484,11 @@ sap.ui.define(
                 oRouter.navTo("StockBinQueryByBin");
 
             },
+            onReceivingofHUbyDoor: function () {
+                var oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("ReceivingOfHuByDoor");
+            },
+            
         });
     }
 );
