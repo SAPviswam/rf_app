322fc1fcf8e7d6677c23dfb9790651dc3dcb2e37
Nigama Ambala <nigama.ambala@artihcus.com>
2024-09-09 09:48:34 +0000

    Receving Of HU BY Delivery

--- webapp/controller/Supervisor.controller.js
+++ webapp/controller/Supervisor.controller.js
@@ -1484,6 +1484,11 @@ sap.ui.define(
                 oRouter.navTo("StockBinQueryByBin");
 
             },
+            onReceivingofHUbyDelivery: function () {
+                var oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("RecevingOfHUbyDelivery");
+
+            },
         });
     }
 );
