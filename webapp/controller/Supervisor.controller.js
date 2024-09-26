3847a0447680d8444e2ab9057d37f93f4f618b42
Ramya Pasala <ramya.pasala@artihcus.com>
2024-09-26 07:13:17 +0000

    updated supervisor view

--- webapp/controller/Supervisor.controller.js
+++ webapp/controller/Supervisor.controller.js
@@ -72,14 +72,14 @@ sap.ui.define(
 
             // Palette button press logic (this is triggered when the button is pressed)
             onPaletteIconPress: function (oEvent) {
-                debugger
+                 
                 // Open the theme dialog box
                 this._currentTileId = oEvent.getSource().getId();
                 this.byId("themeTileDialog").open();
                
             },
             onAfterRendering: function () {
-                debugger
+                 
                 // Apply stored theme color immediately
                 var sStoredThemeColor = localStorage.getItem("themeColor");
                 if (sStoredThemeColor) {
@@ -130,7 +130,7 @@ sap.ui.define(
                 this.byId("themeTileDialog").open();
             },
             onApplyColor: function () {
-                debugger
+                 
                 var oView = this.getView();
                 var oColorPicker = oView.byId("colorPicker");
                 var sColorPickerValue = oColorPicker.getColorString();
@@ -197,7 +197,7 @@ sap.ui.define(
                 this.byId("colorPicker").setVisible(!oSelectedCheckBox.getSelected());
             },
             applyThemeColor: function (sColor) {
-                debugger
+                 
                 var aElements = [
                     this.byId("toolPage"),
                     this.byId("idSideNavigation"),
@@ -229,7 +229,7 @@ sap.ui.define(
                 localStorage.setItem("themeColor", sColor);
             },
             applyColorToTile: function (sTileId, sColor) {
-                debugger
+                 
                 var oTile = this.byId(sTileId);
 
                 if (!oTile) return; // If tile doesn't exist, return early
@@ -342,7 +342,7 @@ sap.ui.define(
                 }
             },
             onApproveUserBtnPress: async function () {
-                debugger
+                 
                 var oView = this.getView();
                 if (this.byId("idRequestedData").getSelectedItems().length < 1) {
                     MessageToast.show("Please Select atleast one Resource");
@@ -424,7 +424,7 @@ sap.ui.define(
                 this.oApproveForm.close();
             },
             onApprove: function () {
-                debugger
+                 
                 var Empid = this.byId("idEmployeeIDInputF").getText();
 
                 var oNameInput = this.byId("idNameInputF");
@@ -673,7 +673,7 @@ sap.ui.define(
             },
 
             onCheckBoxSelect: function () {
-                debugger
+                 
                 // Get the MultiComboBox instance for the Process Area
                 var oMultiComboBox = this.byId("AreaSelect");
 
@@ -781,7 +781,7 @@ sap.ui.define(
                 this.getView().byId("_IDGenTswfd_able1").setVisible(true);
             },
             onCheckBoxSelectGroup: function () {
-                debugger;
+                 ;
 
                 // Get the MultiComboBox instances for Area and Group
                 var oAreaMultiComboBox = this.byId("AreaSelect");
@@ -894,7 +894,7 @@ sap.ui.define(
                 });
             },
             onCheckBoxSelectQueue: function () {
-                debugger;
+                 ;
 
                 // Get the MultiComboBox instances for Group and Queue
                 var oGroupMultiComboBox = this.byId("GroupSelect");
@@ -978,7 +978,7 @@ sap.ui.define(
 
 
             onApprovePress: function () {
-                debugger
+                 
                 var Empid = this.byId("idEmppInput").getValue();
 
                 var oNameInput = this.byId("idNameInput");
@@ -1026,7 +1026,6 @@ sap.ui.define(
                     oEmailInput.setValueState(sap.ui.core.ValueState.None);
                     oEmailInput.setValueStateText("");
                 }
-                debugger
                 // Validate Phone
                 if (!phone) {
                     oPhoneInput.setValueState(sap.ui.core.ValueState.Error);
@@ -1221,7 +1220,6 @@ sap.ui.define(
             },
 
             onEmployeeIdLiveChange: function (oEvent) {
-                debugger
                 var oInput = oEvent.getSource();
                 var Empid = oInput.getValue();
                 var oModel = this.getOwnerComponent().getModel();  // Assumes you have set the model to your view
@@ -1573,7 +1571,6 @@ sap.ui.define(
                 // }
             },
             OnPressStockBinQueryByBin: function () {
-                debugger
                 var oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("StockBinQueryByBin", { id: this.ID });
 
@@ -1648,7 +1645,7 @@ sap.ui.define(
 
             },
             onChangeQueueTilePress: function () {
-                debugger
+                 
                 var oRouter = this.getOwnerComponent().getRouter();
                 oRouter.navTo("ChangeQueue", { id: this.ID });
 
@@ -1987,7 +1984,7 @@ sap.ui.define(
  
             },
             onUnloadingBYASN: function () {
-                debugger
+                 
                 var oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("RouteUnloadingASNDetails", { id: this.ID });
             },
@@ -2006,7 +2003,7 @@ sap.ui.define(
                 oRouter.navTo("CreatePutawayHusManually",{id:this.ID});
             },
             onManuallyRepackHUItemPress: function () {
-                debugger
+                 
                 var oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("ManuallyRepackingByHUItem", { id: this.ID });
             },
@@ -2044,7 +2041,7 @@ sap.ui.define(
                 oRouter.navTo("RoutePutawayHuClustered",{id:this.ID});
             },
             onPressReversalofConsumptionbyMO_Bin:function() {
-                debugger
+                 
                 var oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("RouteReversalofConsumptionbyMO_Bin",{id:this.ID});
             },
