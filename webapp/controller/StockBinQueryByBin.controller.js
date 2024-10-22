c5d4c015d53b95cd949e110a9abb90e4bde441f2
Devendra Goud <devendra.goud@artihcus.com>
2024-10-22 12:17:49 +0000

    Bin Query by Product(Final)

--- webapp/controller/StockBinQueryByBin.controller.js
+++ webapp/controller/StockBinQueryByBin.controller.js
@@ -54,13 +54,13 @@ sap.ui.define([
     // },
     onScanSuccess: function (oEvent) {
       // Get the scanned bin number from the event
-      var sScannedBinNumber = oEvent.getParameter("text"); // Assuming this is how you get the scanned text
+      var sScannedBinNumber = oEvent.getParameter("text"); 
 
       // Set the scanned value into the input field
       this.getView().byId("_IDBinGenInput1_SBQB").setValue(sScannedBinNumber);
 
       // Call the submit function to fetch products
-      this.onPressBinSubmit(); // Call your existing submit function
+      this.onPressBinSubmit(); 
     },
     onPressBinSubmit: function () {
       // Get the input value from the input field
@@ -152,8 +152,6 @@ sap.ui.define([
         },
 
         success: function (odata) {
-          console.log(odata)
-
           var oView = that.getView();
           oView.byId("idSBQBBinInput").setValue(odata.Lgtyp);
           oView.byId("idSBQBMaxVInput").setValue(odata.MaxVolume);
