5dbd2d967105d8eb733f98d7e66906c477516511
Pasuvula Ramesh <ramesh.pasuvula@artihcus.com>
2024-11-14 11:03:28 +0000

    Modified Avatar Press function in Every controller

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -72,7 +72,20 @@ sap.ui.define([
             this.getView().addDependent(oFragment);
             return oFragment
         },
+        onPressAvatarEveryTileHelperFunction: function (oEvent) {
+            var oComponent = this.getOwnerComponent();
 
+            // Destroy the existing popover if it exists
+            if (oComponent.getPopover()) {
+                oComponent.getPopover().destroy();
+                oComponent.setPopover(null);
+            }
+            this.onPressAvatarPopOverBaseFunction(oEvent, {
+                showAccountDetails: true,
+                showSignOut: true
+            });
+            this.applyStoredProfileImage();
+        },
         //Base function for opening the Profile PopOver..
         onPressAvatarPopOverBaseFunction: function (oEvent, oPopoverContext) {
             debugger;
