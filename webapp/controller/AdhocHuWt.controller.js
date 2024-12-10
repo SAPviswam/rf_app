e98cffd35ca08731bc04e1d9f74636fabc625f10
Nigama Ambala <nigama.ambala@artihcus.com>
2024-12-10 06:47:59 +0000

    Added AdhocHuWT View

--- webapp/controller/AdhocHuWt.controller.js
+++ webapp/controller/AdhocHuWt.controller.js
@@ -1,58 +1,75 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-     "sap/m/MessageToast",
-         "sap/ui/core/UIComponent"
+    "sap/m/MessageToast",
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device"
 ],
-function (Controller,MessageToast,UIComponent) {
-    "use strict";
- 
-    return Controller.extend("com.app.rfapp.controller.AdhocHuWt", {
-        onInit: function () {
-            const oRouter = this.getOwnerComponent().getRouter();
-          oRouter.attachRoutePatternMatched(this.onResourceDetailsLoad, this);
-        },
-        onResourceDetailsLoad:function(oEvent1){
-            var that = this;
-            const { id } = oEvent1.getParameter("arguments");
-            this.ID = id;
-            console.log(this.ID);
-        },
-        onPressSubmitInAdhocHuWt: function () {
- 
-            this.getView().byId("idHuScanning").setVisible(false);
-            this.getView().byId("idHuDetails").setVisible(true);
-            this.getView().byId("idBackButtoninAdhocHuWtHuScan").setVisible(false);
-            this.getView().byId("idBackButtoninAdhocHuWtHuDetails").setVisible(true);
- 
-        },
-        onBeforeRendering : function () {
-            this.getView().byId("idBackButtoninAdhocHuWtHuScan").setVisible(true);
-        },
-        onPressBackButtoninAdhocHuWtHuDetails: function () {
- 
-            this.getView().byId("idHuScanning").setVisible(true);
-            this.getView().byId("idHuDetails").setVisible(false);
-            this.getView().byId("idBackButtoninAdhocHuWtHuDetails").setVisible(false);
-            this.getView().byId("idBackButtoninAdhocHuWtHuScan").setVisible(true);
-        },
-        onPressBackButtoninAdhocHuWtHuScan : async function(){
-            var oRouter = UIComponent.getRouterFor(this);
+    function (Controller,MessageToast,UIComponent,Device) {
+        "use strict";
+
+        return Controller.extend("com.app.rfapp.controller.AdhocHuWt", {
+            onInit: function () {
+                const oRouter = this.getOwnerComponent().getRouter();
+                oRouter.attachRoutePatternMatched(this.onResourceDetailsLoad, this);
+                if (Device.system.phone) {
+                    this.getView().byId("idTitle_CAHU").addStyleClass("titleMobile");
+                }
+            },
+            onResourceDetailsLoad: async function (oEvent1) {
+                const { id } = oEvent1.getParameter("arguments");
+                this.ID = id;
+            },
+
+            onLiveChange: function () { 
+                //   if(this.getView().byId("idHuInput_CAHU").getValue()=="800020"){
+                this.getView().byId("idFirstSc_CAHU").setVisible(false)
+                this.getView().byId("idsecondSc_CAHU").setVisible(true)
+                var ohu = this.getView().byId("idHuInput_CAHU").getValue();
+                this.getView().byId("idSecSCHuInput_CAHU").setValue(ohu)
+                this.getView().byId("idSecSCHuInput_CAHU").setEditable(false);
+
+
+                //   }
+                //   else{
+                //       MessageToast.show("please enter valid HU Number")
+                //   }
+            },
+            onHuDetailsPress_CAHU: function () {
+                this.getView().byId("idthirdSc_CAHU").setVisible(true);
+                this.getView().byId("idsecondSc_CAHU").setVisible(false);
+
+            },
+            onSecondBackBtnPress_CAHU: function () {
+                this.getView().byId("idFirstSc_CAHU").setVisible(true);
+                this.getView().byId("idsecondSc_CAHU").setVisible(false);
+
+            },
+            onThirdBackBtnPress_CAHU: function () {
+                this.getView().byId("idsecondSc_CAHU").setVisible(true);
+
+                this.getView().byId("idFirstSc_CAHU").setVisible(false);
+
+
+            },
+            onFirstBackBtnPress_CAHU: async function () {
+                var oRouter = UIComponent.getRouterFor(this);
                 var oModel1 = this.getOwnerComponent().getModel();
                 await oModel1.read("/RESOURCESSet('" + this.ID + "')", {
                     success: function (oData) {
-                        let oUser=oData.Users.toLowerCase()
-                        if(oUser ===  "resource"){
-                            oRouter.navTo("RouteResourcePage",{id:this.ID});
+                        let oUser = oData.Users.toLowerCase()
+                        if (oUser === "resource") {
+                            oRouter.navTo("RouteResourcePage", { id: this.ID });
                         }
-                        else{
-                        oRouter.navTo("Supervisor",{id:this.ID});
-                    }
+                        else {
+                            oRouter.navTo("Supervisor", { id: this.ID });
+                        }
                     }.bind(this),
                     error: function () {
                         MessageToast.show("User does not exist");
                     }
                 });
-        }
- 
-    });
-});
+            },
+
+        });
+    }
+);
