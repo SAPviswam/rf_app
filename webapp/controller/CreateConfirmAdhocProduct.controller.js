84a991d2d91525a522d330267d906261a97f7be4
srilekha paloju <srilekha.paloju@artihcus.com>
2024-12-20 09:11:42 +0000

    added one more function logic in CCAP

--- webapp/controller/CreateConfirmAdhocProduct.controller.js
+++ webapp/controller/CreateConfirmAdhocProduct.controller.js
@@ -47,11 +47,46 @@ sap.ui.define(
                 this.getView().byId("idInitialAdhocProductbackbtn").setVisible(false);
                 this.getView().byId("idproductInput1").setValue(sProductnumber);
             },
-            onSecondSubmitBtnPress: function () {
-                this.getView().byId("idthirdProductPage").setVisible(true);
-                this.getView().byId("idsecondProductPage").setVisible(false);
-                this.getView().byId("idProdutSecondbackbtn").setVisible(true);
-                this.getView().byId("idProductfirstbackbtn").setVisible(false);
+            onSecondSubmitBtnPress: async function () {
+                var oView = this.getView();
+                var sBinnumber = oView.byId("idProductsrcBinInput").getValue();
+                var sProctyp = oView.byId("idProcesstypeInput").getValue();
+
+ 
+                // Convert the product number to uppercase and store it
+                sBinnumber = sBinnumber.toUpperCase();
+                this.sBinnumber = sBinnumber;
+ 
+                // Check if the product number is provided, if not show a message
+                if (!sBinnumber || !sProctyp) {
+                    sap.m.MessageToast.show("Please enter a BinNumber and Process Type");
+                    return;
+                }
+                var oModel = this.getView().getModel(); // Get the model associated with the view
+                var that = this;
+
+                var sRequestUrl = `/ProductWTC1Set(Matnr40='${this.sProductnumber}',Procty='${sProctyp}',Vlpla='${this.sBinnumber}')`;
+
+                await oModel.read(sRequestUrl, {
+                    urlParameters: {
+                        "$expand": "ProductWTCSetsNav1", // Expand related task data
+                        "$format": "json" // Request the data in JSON format
+                    },
+                     success: (odata) => {
+                         console.log(odata);
+     
+                             if (odata.Mantnr40 === that.sProductnumber && odata.Procty === sProctyp && odata.vlpla === sBinnumber) {
+                                that.getView().byId("idProductSrcBinInput2").setValue(sBinnumber);
+                                that.getView().byId("idProductInput2").setValue(that.sProductnumber);  
+                         }
+                         that.getView().byId("idthirdProductPage").setVisible(true);
+                         that.getView().byId("idsecondProductPage").setVisible(false);
+                         that.getView().byId("idProdutSecondbackbtn").setVisible(true);
+                         that.getView().byId("idProductfirstbackbtn").setVisible(false);
+                     },
+                    error: function () {
+                    }
+                });
             },
             onProductfirstBackBtnPress: function () {
                 this.getView().byId("idInitialProductPage").setVisible(true);
