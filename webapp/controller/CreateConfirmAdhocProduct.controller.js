06cdeb4d333239967d748ace323a7ed63a14b657
srilekha paloju <srilekha.paloju@artihcus.com>
2024-12-20 12:29:05 +0000

    added controller logic for CCAP

--- webapp/controller/CreateConfirmAdhocProduct.controller.js
+++ webapp/controller/CreateConfirmAdhocProduct.controller.js
@@ -52,11 +52,10 @@ sap.ui.define(
                 var sBinnumber = oView.byId("idProductsrcBinInput").getValue();
                 var sProctyp = oView.byId("idProcesstypeInput").getValue();
 
- 
                 // Convert the product number to uppercase and store it
                 sBinnumber = sBinnumber.toUpperCase();
                 this.sBinnumber = sBinnumber;
- 
+
                 // Check if the product number is provided, if not show a message
                 if (!sBinnumber || !sProctyp) {
                     sap.m.MessageToast.show("Please enter a BinNumber and Process Type");
@@ -65,28 +64,93 @@ sap.ui.define(
                 var oModel = this.getView().getModel(); // Get the model associated with the view
                 var that = this;
 
-                var sRequestUrl = `/ProductWTC1Set(Matnr40='${this.sProductnumber}',Procty='${sProctyp}',Vlpla='${this.sBinnumber}')`;
+                var sRequestUrl = `/ProductWTC1Set(Matnr40='${this.sProductnumber}',Procty='${sProctyp}',Vlpla='${sBinnumber}')`;
 
                 await oModel.read(sRequestUrl, {
                     urlParameters: {
                         "$expand": "ProductWTCSetsNav1", // Expand related task data
                         "$format": "json" // Request the data in JSON format
                     },
-                     success: (odata) => {
-                         console.log(odata);
-     
-                             if (odata.Mantnr40 === that.sProductnumber && odata.Procty === sProctyp && odata.vlpla === sBinnumber) {
+                    success: (odata) => {
+                        console.log(odata);
+                        this.Odata = odata
+                        if (odata.Matnr40 === that.sProductnumber && odata.Procty === sProctyp && odata.Vlpla === sBinnumber) {
+                            var odataItems = odata.ProductWTCSetsNav1.results;
+                            if (odataItems.length > 1) {
+                                // Prepare an array for binding
+                                var aProductDetails = [];
+
+                                // Loop through the results and push them into the array
+                                for (var i = 0; i < odataItems.length; i++) {
+                                    if (odataItems[i].Matnr40) {
+                                        aProductDetails.push({
+                                            Matnr40: odataItems[i].Matnr40,
+                                            Huident: odataItems[i].Huident,
+                                            Owner: odataItems[i].Owner
+                                        });
+                                    }
+                                }
+
+                                // Create a JSON model with the product details array
+                                var oProductModel = new sap.ui.model.json.JSONModel({ products: aProductDetails });
+
+                                // Set the model to the table
+                                that.byId("idBinNumTable_CCAP").setModel(oProductModel);
+
+                                // Bind the items aggregation of the table to the products array in the model
+                                that.byId("idBinNumTable_CCAP").bindItems({
+                                    path: "/products",
+                                    template: new sap.m.ColumnListItem({
+                                        cells: [
+                                            new sap.m.Text({ text: "{Matnr40}" }),  // Product Number
+                                            new sap.m.Text({ text: "{Huident}" }),   // Hu number
+                                            new sap.m.Text({ text: "{Owner}" })   // Owner
+                                        ],
+                                    })
+                                });
+                                that.getView().byId("idProductfirstbackbtn").setVisible(false);
+                                that.getView().byId("idsecondProductPage").setVisible(false);
+                                that.getView().byId("idfifthProductPage").setVisible(true);
+                                that.getView().byId("idBtnHUContentBtn_CCAP").setVisible(true);
+                            }
+                            else {
                                 that.getView().byId("idProductSrcBinInput2").setValue(sBinnumber);
-                                that.getView().byId("idProductInput2").setValue(that.sProductnumber);  
-                         }
-                         that.getView().byId("idthirdProductPage").setVisible(true);
-                         that.getView().byId("idsecondProductPage").setVisible(false);
-                         that.getView().byId("idProdutSecondbackbtn").setVisible(true);
-                         that.getView().byId("idProductfirstbackbtn").setVisible(false);
-                     },
+                                that.getView().byId("idProductInput2").setValue(that.sProductnumber);
+                                that.getView().byId("idthirdProductPage").setVisible(true);
+                                that.getView().byId("idsecondProductPage").setVisible(false);
+                                that.getView().byId("idProdutSecondbackbtn").setVisible(true);
+                                that.getView().byId("idProductfirstbackbtn").setVisible(false);
+                                that.getView().byId("idProductAvlQtyInput2").setValue(odataItems[0].AvailQuan);
+                            }
+                        }
+                    },
                     error: function () {
                     }
                 });
+            },
+            onSelectionChange: function (oEvent) {
+
+                var aMaterials = this.Odata.ProductWTCSetsNav1.results;
+
+                var sSelectedMatnr = oEvent.getSource().getSelectedItem().getBindingContext().getProperty("Matnr40");
+                var sSelectedHuident = oEvent.getSource().getSelectedItem().getBindingContext().getProperty("Huident");
+
+                var oSelectedMaterial = aMaterials.find(function (material) {
+
+                    return material.Matnr40 === sSelectedMatnr && material.Huident === sSelectedHuident;
+                });
+                if (oSelectedMaterial) {
+                    this.getView().byId("idthirdProductPage").setVisible(true);
+                    this.getView().byId("idsecondProductPage").setVisible(false);
+                    this.getView().byId("idProdutSecondbackbtn").setVisible(true);
+                    this.getView().byId("idProductfirstbackbtn").setVisible(false);
+                    this.getView().byId("idProductAvlQtyInput2").setValue(oSelectedMaterial.AvailQuan);
+                    this.getView().byId("idProductSrcBinInput2").setValue(this.sBinnumber);
+                    this.getView().byId("idProductInput2").setValue(this.sProductnumber);
+
+                } else {
+                    sap.m.MessageToast.show("Material not found.");
+                }
             },
             onProductfirstBackBtnPress: function () {
                 this.getView().byId("idInitialProductPage").setVisible(true);
@@ -100,24 +164,16 @@ sap.ui.define(
                 this.getView().byId("idProductfirstbackbtn").setVisible(true);
                 this.getView().byId("idProdutSecondbackbtn").setVisible(false)
             },
-            onThirdSubmitBtnPress:function() {
-                this.getView().byId("idsecondProductPage").setVisible(false);
-                this.getView().byId("idthirdProductPage").setVisible(false);
+            onbackbuttonfromtablepress: function () {
                 this.getView().byId("idProductfirstbackbtn").setVisible(true);
-                this.getView().byId("idProdutSecondbackbtn").setVisible(false);
-                this.getView().byId("idfifthProductPage").setVisible(true)
-
-            },
-            onbackbuttonfromtablepress:function() {
-                this.getView().byId("idsecondProductPage").setVisible(false);
-                this.getView().byId("idthirdProductPage").setVisible(true);
-                this.getView().byId("idProductfirstbackbtn").setVisible(true);
-                this.getView().byId("idProdutSecondbackbtn").setVisible(false);
+                this.getView().byId("idsecondProductPage").setVisible(true);
                 this.getView().byId("idfifthProductPage").setVisible(false);
                 this.getView().byId("idfourthProductPage").setVisible(false)
-
             },
             onProductdetailsBtnPress: function () {
+                var aProductdata = this.Odata.ProductWTCSetsNav1.results[0];
+                
+
                 this.getView().byId("idfourthProductPage").setVisible(true);
                 this.getView().byId("idthirdProductPage").setVisible(false);
                 this.getView().byId("idProductthirdbackbtn").setVisible(true);
